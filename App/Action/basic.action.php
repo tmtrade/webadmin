12c282abae68bc25cae098e326581c7f62faaec2
xuni <xuni@8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4>
2016-02-17 11:12:54 +0000

    提交 首页基本配置 页面
    git-svn-id: http://192.168.0.5/svn/Member/trade/webadmin@7703 8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4

---
+++ App/Action/basic.action.php
@@ -0,0 +1,29 @@
+<?
+/**
+ * 首页基本配置
+ *
+ * @package	Action
+ * @author	Xuni
+ * @since	2016-02-17
+ */
+class BasicAction extends AppAction
+{
+//	public $debug = true;
+	
+	/**
+	 * 国内商标列表
+	 * 
+	 * @author	Xuni
+	 * @since	2016-01-8
+	 * @access	public
+	 * @return	void
+	 */
+	public function index()
+	{	
+		
+		$this->display();
+	}
+
+
+}
+?>

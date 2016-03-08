b5f59ab072d2b3dd59f49329bc770d69c24b1451
martin <martin@8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4>
2016-03-08 04:41:54 +0000

    测试
    git-svn-id: http://192.168.0.5/svn/Member/trade/webadmin@8046 8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4

--- App/Util/ucClient.php
+++ App/Util/ucClient.php
@@ -30,7 +30,9 @@ class ucClient
             $param = array_merge($extra, $param);
         }
         $url 	= NAV_SSO_HOST.$type.'/?'.http_build_query($param);
+		echo $url;
         $result = file_get_contents($url);
+		print_r($result);exit;
         return $result;
     }
     /**

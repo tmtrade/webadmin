4dd04836772aa9beceaf85f448a30a87e5b7070a
dower <dower@8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4>
2016-05-17 02:22:50 +0000

    添加统计相关表模型
    git-svn-id: http://192.168.0.5/svn/Member/trade/webadmin@9728 8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4

--- Config/Table/table.route.config.php
+++ Config/Table/table.route.config.php
@@ -1,7 +1,3 @@
 <?
-
-require('trade.table.config.php');//老数据库
-require('trade_new.table.config.php');//新数据库
-require('trademarkonline.table.config.php');
-
+require('trade_new.table.config.php');
 ?>

2e1cabd3971922becb924cfc7287ecdbcd6188f6
dower <dower@8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4>
2016-05-26 10:35:32 +0000

    统计添加webid
    git-svn-id: http://192.168.0.5/svn/Member/trade/webadmin@9970 8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4

--- Config/visitlog.config.php
+++ Config/visitlog.config.php
@@ -233,5 +233,121 @@ return $frequency = array(
             ),
         ),
     ),
+    "8" => array(
+        'title' => '专题列表页',
+        "web_type" => "8",
+        'view' => array(
+            '1' => array(
+                'title' => '查看zhuanti',
+                'web_id' => '1',
+            ),
+        ),
+    ),
+    "9" => array(
+        'title' => '专题详情页',
+        "web_type" => "9",
+        'view' => array(
+            '1' => array(
+                'title' => '询价',
+                'web_id' => '1',
+            ),
+            '2' => array(
+                'title' => '联系卖家',
+                'web_id' => '2',
+            ),
+        ),
+    ),
+    "10" => array(
+        'title' => '专利详情页',
+        "web_type" => "10",
+        'view' => array(
+            '1' => array(
+                'title' => '查看价格',
+                'web_id' => '1',
+            ),
+            '2' => array(
+                'title' => '在线咨询',
+                'web_id' => '2',
+            ),
+            '3' => array(
+                'title' => '更多专利',
+                'web_id' => '3',
+            ),
+        ),
+    ),
+    "11" => array(
+        'title' => '专利列表',
+        "web_type" => "11",
+        'view' => array(
+            '1' => array(
+                'title' => '筛选分类',
+                'web_id' => '1',
+            ),
+            '2' => array(
+                'title' => '筛选群组',
+                'web_id' => '2',
+            ),
+            '3' => array(
+                'title' => '清楚筛选条件',
+                'web_id' => '3',
+            ),
+            '4' => array(
+                'title' => '询价',
+                'web_id' => '4',
+            ),
+            '5' => array(
+                'title' => '联系卖家',
+                'web_id' => '5',
+            ),
+            '6' => array(
+                'title' => '更多专利',
+                'web_id' => '6',
+            ),
+        ),
+    ),
+    "12" => array(
+        'title' => '我要买',
+        "web_type" => "12",
+        'view' => array(
+            '1' => array(
+                'title' => '提交数据',
+                'web_id' => '1',
+            ),
+        ),
+    ),
+    "13" => array(
+        'title' => '我要卖',
+        "web_type" => "13",
+        'view' => array(
+            '1' => array(
+                'title' => '提交数据',
+                'web_id' => '1',
+            ),
+        ),
+    ),
+    "14" => array(
+        'title' => '商品聚合页',
+        "web_type" => "14",
+        'view' => array(
+            '1' => array(
+                'title' => '询价',
+                'web_id' => '1',
+            ),
+            '2' => array(
+                'title' => '联系卖家',
+                'web_id' => '2',
+            ),
+        ),
+    ),
+    "15" => array(
+        'title' => '专利出售',
+        "web_type" => "15",
+        'view' => array(
+            '1' => array(
+                'title' => '提交数据',
+                'web_id' => '1',
+            ),
+        ),
+    ),
 );
 ?>

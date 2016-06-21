7f6517b4dcbe89cdca705fe81d0d9ff07f0e4413
xuni <xuni@8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4>
2016-06-21 07:44:21 +0000

    修改API地址
    git-svn-id: http://192.168.0.5/svn/Member/trade/webadmin@10446 8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4

--- Config/Extension/service.config.php
+++ Config/Extension/service.config.php
@@ -2,6 +2,6 @@
 //api接口配置信息
 $config  = array(
 	'2' => array('url' => 'http://api2.chofn.net/', 'token' => '*'),	//超凡帐户中心URL
-    '3' => array('url' => 'http://uc.test.chofn.net/', 'token' => '*'),    //用户中心接口URL
+    '3' => array('url' => 'http://my.test.chofn.net/', 'token' => '*'),    //用户中心接口URL
 );
 ?>

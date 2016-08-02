dd78b5fde7c1cfb0db547bf3851b79a84ff9b1ec
far <far@8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4>
2016-08-02 09:46:27 +0000

    添加手机端数据统计配置
    git-svn-id: http://192.168.0.5/svn/Member/trade/webadmin@11435 8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4

---
+++ Config/visitlog_wap.config.php
@@ -0,0 +1,196 @@
+<?
+/**
+ * 定义页面和页面模块的属性ID
+ */
+return $frequency = array(
+    "1" => array(
+        'title' => '首页',
+        "web_type" => "1",
+        'view' => array(
+            '1' => array(
+                'title' => 'BANNER',
+                'web_id' => '1',
+            ),
+            '2' => array(
+                'title' => '搜索商标',
+                'web_id' => '2',
+            ),
+            '3' => array(
+                'title' => '搜索专利',
+                'web_id' => '3',
+            ),
+            '4' => array(
+                'title' => '热门关键字商标',
+                'web_id' => '4',
+            ),
+            '5' => array(
+                'title' => '热门关键字专利',
+                'web_id' => '5',
+            ),
+            '6' => array(
+                'title' => '图标商标筛选',
+                'web_id' => '6',
+            ),
+            '7' => array(
+                'title' => '图标专利筛选',
+                'web_id' => '7',
+            ),
+            '8' => array(
+                'title' => '图标购买登记',
+                'web_id' => '8',
+            ),
+            '9' => array(
+                'title' => '图标我要出售',
+                'web_id' => '9',
+            ),
+            '10' => array(
+                'title' => '热门商标类别',
+                'web_id' => '10',
+            ),
+            '11' => array(
+                'title' => '热门专利类别',
+                'web_id' => '11',
+            ),
+            '31' => array(
+                'title' => '在线咨询',
+                'web_id' => '31',
+            ),
+        ),
+    ),
+    "2" => array(
+        'title' => '商标筛选页',
+        "web_type" => "2",
+        'view' => array(
+            '1' => array(
+                'title' => '筛选分类',
+                'web_id' => '1',
+            ),
+            '2' => array(
+                'title' => '筛选组合类型',
+                'web_id' => '2',
+            ),
+            '3' => array(
+                'title' => '筛选商标字数',
+                'web_id' => '3',
+            ),
+            '4' => array(
+                'title' => '进入详情',
+                'web_id' => '4',
+            ),
+            '30' => array(
+                'title' => '立即询价',
+                'web_id' => '30',
+            ),
+        ),
+    ),
+    "3" => array(
+        'title' => '专利筛选页',
+        "web_type" => "3",
+        'view' => array(
+            '1' => array(
+                'title' => '专利类型',
+                'web_id' => '1',
+            ),
+            '2' => array(
+                'title' => '类型分类',
+                'web_id' => '2',
+            ),
+            '3' => array(
+                'title' => '进入详情',
+                'web_id' => '3',
+            ),
+            '30' => array(
+                'title' => '立即询价',
+                'web_id' => '30',
+            ),
+        ),
+    ),
+    "4" => array(
+        'title' => '商标详情页',
+        "web_type" => "4",
+        'view' => array(
+            '1' => array(
+                'title' => '推荐模块',
+                'web_id' => '1',
+            ),
+            '30' => array(
+                'title' => '立即询价',
+                'web_id' => '30',
+            ),
+            '2' => array(
+                'title' => '立即购买',
+                'web_id' => '2',
+            ),
+            '3' => array(
+                'title' => '分享',
+                'web_id' => '3',
+            ),
+        ),
+    ),
+    "5" => array(
+        'title' => '专利详情页',
+        "web_type" => "5",
+        'view' => array(
+            '1' => array(
+                'title' => '推荐模块',
+                'web_id' => '1',
+            ),
+            '30' => array(
+                'title' => '立即询价',
+                'web_id' => '30',
+            ),
+            '2' => array(
+                'title' => '立即购买',
+                'web_id' => '2',
+            ),
+            '3' => array(
+                'title' => '分享',
+                'web_id' => '3',
+            ),
+        ),
+    ),
+    
+    "100" => array(
+        'title' => '通用',
+        "web_type" => "100",
+        'view' => array(
+            '100' => array(
+                'title' => '导航商标',
+                'web_id' => '100',
+            ),
+            '101' => array(
+                'title' => '导航专利',
+                'web_id' => '101',
+            ),
+            '102' => array(
+                'title' => '导航客服',
+                'web_id' => '102',
+            ),
+            '103' => array(
+                'title' => '导航菜单',
+                'web_id' => '103',
+            ),
+            '104' => array(
+                'title' => '一只蝉首页',
+                'web_id' => '104',
+            ),
+            '105' => array(
+                'title' => '我要出售 ',
+                'web_id' => '105',
+            ),
+            '106' => array(
+                'title' => '购买登记',
+                'web_id' => '106',
+            ),
+            '107' => array(
+                'title' => '我的蝉窝',
+                'web_id' => '107',
+            ),
+            '108' => array(
+                'title' => '出售者平台',
+                'web_id' => '108',
+            ),
+        ),
+    ),
+);
+?>

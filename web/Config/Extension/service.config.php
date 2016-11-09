a5d41b2c7b89ddae36f2d66d640abf7820075663
xuni <xtvj007@163.com>
2016-11-09 10:22:57 +0800

    delete config
    删除配置文件

--- web/Config/Extension/service.config.php
+++
@@ -1,8 +0,0 @@
-<?
-//api接口配置信息
-$config  = array(
-	'2' => array('url' => 'http://api2.chofn.net/', 'token' => '*'),	//超凡帐户中心URL
-    '3' => array('url' => 'http://my.test.chofn.net/', 'token' => '*'),    //用户中心接口URL
-    '4' => array('url' => 'http://seller.chofn.net/', 'token' => '*'),    //出售者平台报价单接口url
-);
-?>
